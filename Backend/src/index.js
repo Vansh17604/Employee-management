ecf1ff5c9d8c1fcde9d6360c72700dd5272993b8
Vansh17604 <vansh456patel@gmail.com>
2025-07-20 11:03:47 +0530

    littlechang

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -35,8 +35,10 @@ app.use(AadharRoutes);
 app.use(UserRoutes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '../public/uploads')));
-
-
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
